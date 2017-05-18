9fe34f76c3d1d375cd544f8bc0943f8bb2b43892
ZhongzhengXiong <tomcatxiong2@gmail.com>
2017-05-18 17:02:15 +0800

    intialize

---
+++ cmake-build-debug/CMakeFiles/Light_bot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Light_bot.dir/main.cpp.obj"
+  "Light_bot.pdb"
+  "Light_bot.exe"
+  "Light_bot.exe.manifest"
+  "libLight_bot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Light_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Light_bot.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Light_bot.
+# This may be replaced when dependencies are built.
